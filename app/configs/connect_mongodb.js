bf9e819f6450265ac47986d532d9ec49402f9393
chinhkt@vng.com.vn <chinhkt@vng.com.vn>
2025-11-24 00:19:05 +0700

    --

--- app/configs/connect_mongodb.js
+++ app/configs/connect_mongodb.js
@@ -1,13 +1,6 @@
 const mongoose = require("mongoose");
 const constants = require("./constants");
-let envConfigs = {
-	DATABASE_USER: process.env.DATABASE_USER,
-	DATABASE_PASS: process.env.DATABASE_PASS,
-	DATABASE_HOST: process.env.DATABASE_HOST,
-	DATABASE_PORT: process.env.DATABASE_PORT,
-	DATABASE_NAME: process.env.DATABASE_NAME,
-	DATABASE_REPLICASET: process.env.DATABASE_REPLICASET,
-};
+
 const options = {
 	readPreference: "secondaryPreferred", // https://docs.mongodb.com/manual/core/read-preference/#secondaryPreferred
 	maxStalenessSeconds: 120, // Min: 120 // https://docs.mongodb.com/manual/core/read-preference-staleness/#replica-set-read-preference-max-staleness
@@ -20,15 +13,10 @@ const options = {
 	connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
 };
 
-let connectionString = `mongodb://${envConfigs.DATABASE_USER}:${envConfigs.DATABASE_PASS}@${envConfigs.DATABASE_HOST}:${envConfigs.DATABASE_PORT}/${envConfigs.DATABASE_NAME}`;
-if (envConfigs.DATABASE_REPLICASET) {
-	connectionString = `mongodb://${envConfigs.DATABASE_USER}:${envConfigs.DATABASE_PASS}@${envConfigs.DATABASE_REPLICASET}/${envConfigs.DATABASE_NAME}`;
-}
-// console.log(connectionString, "connectionString");
-// mongodb://127.0.0.1:27017/warrior_utc_code
 mongoose.set("strictQuery", false);
+console.log(constants.MongoURL, "constants.MongoURL");
 mongoose
-	.connect(encodeURI(connectionString), options)
+	.connect(encodeURI(constants.MongoURL), options)
 	.then(() => {
 		console.log("\x1b[35m[MongoDB]\x1b[0m MongoDB connected and ready to use...");
 	})
