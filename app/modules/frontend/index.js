13080966bd6d0060159906d4c030cb39e609aac4
chinhkt <chinhkt@vng.com.vn>
2025-11-26 11:06:52 +0700

    --

--- app/modules/frontend/index.js
+++ app/modules/frontend/index.js
@@ -5,6 +5,7 @@ frontend.set("views", _basepath + "app/views/frontend/");
 frontend.get("/", async function (req, res) {
 	try {
 		// res.send("Hello World");
+		return res.redirect("https://zalo.me/s/868938787760554822/");
 		res.render("index");
 	} catch (error) {
 		console.log(error);
